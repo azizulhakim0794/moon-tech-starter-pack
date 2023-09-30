99d9aa6792b37da49dc640924778a53e9dc4d8ee
unknown <azizulhakimtumzid0793@gmail.com>
2023-09-30 06:39:50 +0600

    redux project

---
+++ src/redux/reducer/productReducer.js
@@ -0,0 +1,28 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actionTypes/actionTypes"
+const initialStart = {
+    cart:[]
+};
+
+const productReducer = (state = initialStart , action) =>{
+    const selectedProduct = state.cart.find((product)=> product._id === action.payload._id);
+switch (action.type){
+  
+    case ADD_TO_CART:
+        if(selectedProduct) {
+            return state;
+        }
+        return {
+            ...state,
+            cart: [...state.cart, {...action.payload, quantity:1 }]
+        };
+        case REMOVE_FROM_CART:
+            return {
+                ...state,
+                cart: [state.cart .filter((product)=> product._id !== action.payload._id)]
+            };
+    default:
+        return state
+}
+ };
+
+export default productReducer;
